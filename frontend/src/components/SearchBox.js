81fb55408e8bf04351f44192e44d12f6c657f15e
Pablo <paabloms@live.com>
2018-12-01 04:04:00 -0300

    finalizado autocomplete ajax

--- frontend/src/components/SearchBox.js
+++ frontend/src/components/SearchBox.js
@@ -5,12 +5,17 @@ import axios from 'axios';
 import MenuItem from 'material-ui/MenuItem';
 import AutoComplete from 'material-ui/AutoComplete';
 
+//Adicionar o prefixo --> 'https://cors-anywhere.herokuapp.com/'
 const baseUrl = 'https://api-endpoint.igdb.com/games/?search';
+const baseUrlImage = 'https://images.igdb.com/igdb/image/upload/t';
 
 const styles = {
   SearchIcon: {
     float: 'left',
     paddingTop: 17
+  },
+  span:{
+    float: 'flex'
   },
   img:{
     float: 'left',
@@ -25,71 +30,54 @@ class SearchBox extends React.Component {
 
   constructor(props){
     super(props);
-    this.state = {dataSource: [],
-                };
-  }
-
-  searchGameByName(value){
-    axios.get(`${baseUrl}=${value}&fields=id,name,cover`, {
-      headers: {
-        "user-key": "510724fce12ac8a9d0d97787c5b2e6e5",
-        Accept: "application/json"
-      }
-    })
-    .then(response => {
-      //const dataSourceArr = this.getDataSource(response.data);
-      // this.setState({
-      //   dataSource: [
-      //     {
-      //       text: 'oi',
-      //       value: (
-      //         <MenuItem primaryText="text-value1">
-      //           <img style={styles.img} src="https://unsplash.it/40/40"/>
-      //         </MenuItem>
-      //       ),
-      //     },
-      //     {
-      //       text: 'text-value2',
-      //       value: (
-      //         <MenuItem
-      //           primaryText="text-value2"
-      //           secondaryText="&#9786;"
-      //         />
-      //       ),
-      //     },  
-      //   ]
-      // });
-      const dataSourceArr = this.getDataSource(response.data);
-      this.setState({
-        dataSource: dataSourceArr
-      });
-    })
-    .catch(e => {
-      console.log("error", e);
-    });
+    this.state = {
+      dataSource: [],
+    };
   }
 
   getDataSource(data){
     let arr = [];
     data.forEach(e => {
-      //const url = e.cover.url;
-      let cover = {...e.cover};
-      let obj = {
-        text: e.name,
-        value:(
-          <MenuItem primaryText={e.name}>
-            <img style={styles.img} src={`https:${cover.url}`}/>
-            {/* <img style={styles.img} src=""/> */}
-          </MenuItem>
-        )
-      };
-      arr.push(obj);
+      if(e.cover != undefined){
+        let obj = {
+          text: e.name,
+          value:(
+            <MenuItem primaryText={<span style={styles.span}>{e.name}</span>}>
+              <img style={styles.img} src={`${baseUrlImage}_micro/${e.cover.cloudinary_id}.jpg`}/>
+            </MenuItem>
+          )
+        };
+        arr.push(obj);
+      }
     });
     return arr;
   }
 
   handleUpdateInput(value){
-    this.searchGameByName(value);
+  
+    if (value.length) {
+      axios.get(`${baseUrl}=${value}&fields=id,name,cover`, {
+        headers: {
+          "user-key": "510724fce12ac8a9d0d97787c5b2e6e5",
+          Accept: "application/json"
+        }
+      })
+        .then((res) => this.handleSuccess(res.data),
+              (err) => this.handleFailure(err));
+    } else {
+      this.setState({
+        dataSource: []
+      });
+    }
+
+  }
+  
+  handleSuccess (response) {
+    const arr = this.getDataSource(response);
+    this.setState({dataSource: arr});
+  }
+  handleFailure (err) {
+    console.log(err);
   }
 
   render(){
@@ -98,6 +86,7 @@ class SearchBox extends React.Component {
         <Search style={styles.SearchIcon} color={white} />
         <AutoComplete style={styles.AutoComplete}
           hintText="Search Game"
+          filter={() => true} 
           fullWidth={true}
           dataSource={this.state.dataSource}
           onUpdateInput={value => this.handleUpdateInput(value)}
